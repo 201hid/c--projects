cb9eb109c19a4201f9237327d9ff69b50658cda2
tawhid <ah5229530@gmail.com>
2024-04-18 16:36:39 +1000

    commit
    commit

--- iterator/iterator/FibonacciSequenceIterator.cpp
+++ iterator/iterator/FibonacciSequenceIterator.cpp
@@ -1,5 +1,47 @@
 #include "FibonacciSequenceIterator.h"
+#include <iostream>
 
+FibonacciSequenceIterator::FibonacciSequenceIterator(const FibonacciSequenceGenerator& aSeqObject, long long aStartIndex) noexcept
+    : fSequenceObject(aSeqObject), fIndex(aStartIndex) {
+    for (long long i = 1; i < fIndex; i++) {
+        fSequenceObject.next();
+    }
+}
 
-FibonacciSequenceIterator :: FibonacciSequenceIterator( const FibonacciSequenceGenerator& aSeqObject, long long aStartIndex) noexcept :
-for(long long i=1; i <)
+const long long& FibonacciSequenceIterator::operator*() const noexcept {
+    return *fSequenceObject;
+}
+
+FibonacciSequenceIterator& FibonacciSequenceIterator::operator++() noexcept {
+    fSequenceObject.next();
+    fIndex++;
+    return *this;
+}
+
+FibonacciSequenceIterator FibonacciSequenceIterator::operator++(int) noexcept {
+    FibonacciSequenceIterator lCopy = *this;
+    ++(*this);
+    return lCopy;
+}
+
+bool FibonacciSequenceIterator::operator==(const FibonacciSequenceIterator& aOther) const noexcept {
+    return this->fIndex == aOther.fIndex && fSequenceObject.id() == aOther.fSequenceObject.id();
+}
+
+bool FibonacciSequenceIterator::operator!=(const FibonacciSequenceIterator& aOther) const noexcept {
+    return !(*this == aOther);
+}
+
+FibonacciSequenceIterator FibonacciSequenceIterator::begin() const noexcept {
+    return FibonacciSequenceIterator(fSequenceObject);
+}
+
+FibonacciSequenceIterator FibonacciSequenceIterator::end() const noexcept {
+    FibonacciSequenceIterator lEndIterator = *this;
+    while (lEndIterator.fSequenceObject.hasNext()) {
+        lEndIterator++;
+    }
+
+    return ++lEndIterator;
+
+}
